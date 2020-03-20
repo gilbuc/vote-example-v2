a3227c6e803f578de2eb0f4b05cd40a888449bf7
ubg1 <gilbert.buchner@uniqa.at>
2020-03-20 17:48:03 +0100

    kapitel 5.1

---
+++ hands-on/src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import VotingComponent from "./components/VotingComponent";
+
+const vote = {
+    title : "How is you day?",
+    description : "Tell me: how was your day been so far?",
+    choices : [
+        { id: "choice_1", title: "Good", count:7 },
+        { id: "choice_2", title: "Bad", count:12 },
+        { id: "choice_3", title: "Not sure yet", count:1 }
+    ]
+}
+
+ReactDOM.render(
+    <VotingComponent vote={vote} />,
+    document.getElementById("root")
+);
